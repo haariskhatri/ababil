ffa0bcadcfc9a63b6e7aae0bb5a1e1f30337d9e9
haariskhatri <haarishkkhatri@gmail.com>
2023-04-23 14:18:28 +0530

    coming soon done

---
+++ src/components/comingsoon.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function ComingSoon(){
+    return(
+        <>
+        <div className="welcome">
+        <h1>Coming Soon</h1>
+
+        </div>
+        </>
+    )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+//components
+import ComingSoon from './components/comingsoon';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <ComingSoon/>,
+  },
+  {
+    path: "about",
+    element: <div>About</div>,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <RouterProvider router={router} />
+);
